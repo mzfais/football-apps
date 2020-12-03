165c4bd5a212aabf26c9a9efa19fec38dd4b5378
Ahmad Faisol <mzfais@gmail.com>
2020-12-04 06:24:19 +0700

    Tambah Gibol Applications Class

---
+++ app/src/main/java/id/ac/itn/gibolapps/Gibol.java
@@ -0,0 +1,22 @@
+package id.ac.itn.gibolapps;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+
+public class Gibol extends Application {
+    public static final String CHANNEL_ID = "GibolChannel";
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel myChannel = new NotificationChannel(CHANNEL_ID, "Channel 1", NotificationManager.IMPORTANCE_HIGH);
+            myChannel.setDescription("This Gila Bola Channel");
+            Context context;
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(myChannel);
+        }
+    }
+
+}
